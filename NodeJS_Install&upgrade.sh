a3c0408baf52ec4aefb80e993623b6a6304b4b9d
Tomás <154073319+tomasruizserrano@users.noreply.github.com>
2024-01-12 18:24:52 +0100

    Update NodeJS_Install&upgrade.sh

--- NodeJS_Install&upgrade.sh
+++ NodeJS_Install&upgrade.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# update repositories and upgrade to software to new versions
+sudo apt update -qq && sudo apt -y upgrade 
+
+# install nodeJS and npm
+sudo apt install nodejs npm
+
+# clear the npm cache
+npm cache clean -f 
+
+# install Node’s version manager
+npm install -g n
+
+# Install the latest stable version
+sudo n stable
+
+# Install the latest release
+# sudo n latest
